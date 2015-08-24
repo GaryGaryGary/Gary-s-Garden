4ed59430227326babfbd36b8fe6d9805660329a4
Gary <gary@GarydeMac-mini.local>
2015-08-24 21:47:16 +0800

    test file

---
+++ Gary's first/Gary's first/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gary's first
+//
+//  Created by Gary on 15/8/24.
+//  Copyright (c) 2015年 Gary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gary's first/Gary's first/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gary's first
+//
+//  Created by Gary on 15/8/24.
+//  Copyright (c) 2015年 Gary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
